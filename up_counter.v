3c9287eefb6b2bf658cfd63a10a52dad5caed5c6
Sourav Bhattacharjee <souravbhattacharjee03@gmail.com>
2026-07-10 22:01:44 +0530

    Refactor up_counter module and remove comments

--- up_counter.v
+++ up_counter.v
@@ -1,51 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/11/2026 05:59:31 PM
-// Design Name: 
-// Module Name: up_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module up_counter(
-input clk,rst,
-input ld,
-input [3:0] ldvalue,
-output [3:0] dout
+
+module up_counter(input clk, reset, output[3:0] counter
     );
-reg [3:0] temp;
- 
- initial begin
- temp = 0;
- end
-  always@(posedge clk)
-  begin
-  if(rst == 1'b1)
-  begin 
-  temp <= 4'b0000;
-  end
-  
-  else
-   begin
-   if(ld == 1'b1)
-   temp <= ldvalue;
-   else
-   temp <= temp + 1;
-   end
-    end
-    
-    assign dout = temp;
+reg [3:0] counter_up;
+
+// up counter
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+ counter_up <= 4'd0;
+else
+ counter_up <= counter_up + 4'd1;
+end 
+assign counter = counter_up;
 endmodule
